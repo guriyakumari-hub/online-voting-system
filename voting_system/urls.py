30e2f292108385c3ff9bd0ee0da1a671a96f54ac
Guriya Kumari <guriak9835@gmail.com>
2026-04-14 22:07:43 +0530

    fix login route

--- voting_system/urls.py
+++ voting_system/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
+def home(request):
+    return HttpResponse("Welcome to Online Voting System 🚀")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
